a1f35ae0b55949382af006880512f9cc3bd26114
bharathrames <bharath91505@gmail.com>
2023-08-22 09:12:59 +0530

    changed route

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -8,9 +8,9 @@ export default function Home() {
   const [foodItems, setFoodItems] = useState([])
   const [search, setSearch] = useState('')
   const loadFoodItems = async () => {
-    let response = await fetch("http://localhost:5000/api/auth/foodData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
+    let response = await fetch("https://food-app-backend-iota.vercel.app/api/auth/foodData", {
+      credentials: 'include',
+      Origin:"https://recepieappes.netlify.app/login",
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -7,9 +7,9 @@ export default function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
+    const response = await fetch("https://food-app-backend-iota.vercel.app/api/auth/login", {
+      credentials: 'include',
+      Origin:"https://recepieappes.netlify.app/login",
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
